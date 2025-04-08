45c7bb978b8db487ede07eb6d8797640925cb132
Andy Mean <andymean8@gmail.com>
2025-04-07 22:57:41 -0400

    fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,10 +9,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.act13"
+    namespace = "com.example.prac"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,13 +25,15 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.act13"
-        minSdk = 23
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.prac"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-}
-
+    }
 
     buildTypes {
         release {
